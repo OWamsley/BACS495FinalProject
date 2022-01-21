249a2abe9b4830d13bc90757388fa6969f74b54c
OWamsley <owamsley10@gmail.com>
2022-01-20 18:16:09 -0700

    NavBar Created

---
+++ webforum/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar/index';
+import {BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+    
+      <Navbar/>
+      </Router>
+  );
+}
+
+export default App;
---
+++ webforum/src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElements';
+
+
+const Navbar = () => {
+  return (<>
+      <Nav>
+          <NavLink to="/">
+            <h1>Logo</h1>
+          </NavLink>
+          <Bars/>
+          <NavMenu>
+              <NavLink to="/about" activeStyle>
+                  About
+              </NavLink>
+              <NavLink to="/about" activeStyle>
+                  About
+              </NavLink>
+              <NavLink to="/about" activeStyle>
+                  About
+              </NavLink>
+          </NavMenu>
+          <NavBtn>
+              <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+      </Nav>
+  </>);
+};
+
+export default Navbar;
