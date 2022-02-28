496097b53eddc4b88ae4fdd8214d12123d95cfeb
OWamsley <owamsley10@gmail.com>
2022-02-28 14:07:47 -0700

    Added server

--- webforum/src/App.js
+++ webforum/src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/index';
-import {BrowserRouter as Router } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './Signup';
 
 function App() {
   return (
     <Router>
-    
-      <Navbar/>
-      </Router>
+      <Routes>
+        
+          <Route path="/" element={<DashBoard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        
+      </Routes>
+    </Router>
   );
 }
+
+function DashBoard(){
+  return<>
+  <Navbar/>
+  <h2>Dashboard</h2>
+  </>;
+}
+
+function Login(){
+  return <>
+  <Navbar/>
+  <h2>Login</h2>
+  </>;
+}
+
 
 export default App;
---
+++ webforum/src/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react',
+
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Posts From the express server</h1>
+
+      </div>
+    )
+  }
+
+  callAPI(){
+      fetch()
+
+  }
+}
